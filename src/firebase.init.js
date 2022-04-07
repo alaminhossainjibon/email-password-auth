586858ad0024a689deb4246436ee38b781fa0fe3
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-07 15:03:10 +0600

    email add password varify

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCTl2s8-HPfXy6lgeRUg27wcpw1SHthaTY",
+    authDomain: "email-password-auth-4c9cf.firebaseapp.com",
+    projectId: "email-password-auth-4c9cf",
+    storageBucket: "email-password-auth-4c9cf.appspot.com",
+    messagingSenderId: "879060079821",
+    appId: "1:879060079821:web:260502025fd5d0485a0584"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
